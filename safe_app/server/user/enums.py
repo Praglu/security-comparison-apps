208f7fd72dd366a6304f3f92c05292628ad6b731
Praglu <wojciechpraglowski@gmail.com>
2024-05-09 20:37:27 +0200

    Preparing django app

---
+++ safe_app/server/user/enums.py
@@ -0,0 +1,6 @@
+from enum import Enum
+
+
+class UserGroup(Enum):
+    BASIC = 'basic'
+    MANAGER = 'manager'
--- safe_app/server/user/urls.py
+++ safe_app/server/user/urls.py
@@ -1,8 +1,15 @@
-from django.urls import include, path
+from django.urls import path
 
 from server.user import views
+from server.user.auth import ApiObtainAuthTokenView, ApiRemoveAuthTokenView
+
+
+auth_urlpatterns = [
+    path('obtain-token', ApiObtainAuthTokenView.as_view(), name='obtain_auth_token'),
+    path('remove-token', ApiRemoveAuthTokenView.as_view(), name='remove_auth_token'),
+]
 
 
 urlpatterns = [
     path('', views.index, name='index'),
-]
+] + auth_urlpatterns
